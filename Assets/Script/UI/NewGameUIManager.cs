070d5dd1a1dea39b57f53503a48ca0b6789ff241
WOOD22 <90883790+WOOD22@users.noreply.github.com>
2022-03-21 22:36:29 +0900

    2022.03.21

--- Assets/Script/UI/NewGameUIManager.cs
+++ Assets/Script/UI/NewGameUIManager.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NewGameUIManager : MonoBehaviour
 {
     public static int page = 0;
     public GameObject panel_BackgroundChoice;
     public GameObject text_BackgroundChoice;
+
     private void Start()
     {
         for (int i = 0; i < 4; i++)
